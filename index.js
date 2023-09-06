e18ffa56c54bfd63305326b19f33c560e44fa1f9
babinns <havenamehum@gmail.com>
2023-09-06 20:10:27 -0300

    done

---
+++ index.js
@@ -0,0 +1,8 @@
+const answerButton = document.querySelector(".answer-option");
+const questionContainer = document.querySelector(".question-container")
+
+const question1 = document.getElementById("question1")
+const question2 = document.getElementById("question2")
+const question3 = document.getElementById("question3")
+const question4 = document.getElementById("question4")
+const question5 = document.getElementById("question5")
